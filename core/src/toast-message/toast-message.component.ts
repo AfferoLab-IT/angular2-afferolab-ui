0ec404617f313f8db251d0276535011ca446e35e
Charleston Campos <charleston.campos@afferolab.com.br>
2017-03-10 11:51:29 -0300

    Fix toast message.

--- core/src/toast-message/toast-message.component.ts
+++ core/src/toast-message/toast-message.component.ts
@@ -9,10 +9,16 @@ export class ToastMessageComponent implements OnChanges {
   @Input('message')
   message: string;
 
+  @Input('defaultToastMessage')
+  defaultToastMessage: boolean = true;
+
   ngOnChanges(): void {
-      if (this.message) {
-        Materialize.toast(this.message, 10000);
-      }
+    if (this.message && this.defaultToastMessage) {
+      Materialize.toast(this.message, 10000);
+    }
   }
 
+  showThisMessageInToast(message: string): void {
+    Materialize.toast(message, 10000);
+  }
 }
